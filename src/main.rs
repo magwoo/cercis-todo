f954efccb3f4789e54237a1a6b2bc62634bc8595
magwoo <magwood51@yandex.ru>
2024-05-09 17:25:45 +0300

    added deps and main func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let router = Router::new();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
+
+    axum::serve(listener, router).await?;
+
+    Ok(())
 }
